4b82d81d2b592636bb78afbb3b3166f30ba24d3d
bavreet <bavreets25@ausdk12.org>
2024-08-23 17:04:35 +0000

    save

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,47 @@
+public class BodyOfWater{
+    private String name;
+    private double largestDiameter;
+    private double avgDepth;
+    private boolean isSaltWater;
 
+    public BodyOfWater(string n, double l, double d,boolean s){
+        name = n;
+        largestDiameter = l;
+        avgDepth = d;
+        isSaltWater = s;
+    }
+    public BodyOfWater (string n, double d){
+        name = n;
+        avgDepth = d;
+    }
+
+    //GETTERS
+
+    public void setname( String n){
+        name = n;
+    }
+    public void setlargestDiameter( double l){
+        largestDiameter = l;
+    }
+    public void setavgDepth( String d){
+        avgDepth = d;
+    }
+    public void setisSaltWater( String s){
+        isSaltWater = s;
+    }
+
+
+    public String getname(){
+        return name;
+    }
+    public double getlargestDiameter(){
+        return largestDiameter;
+    }
+    public double getavgDepth(){
+        return avgDepth;
+    }
+    public boolean getisSaltWater(){
+        return isSaltWater;
+    }
+
+}
--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,7 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
+        BodyOfWater b = new BodyOfWater(lake tahoe, 20, 60, true);
+        BodyOfWater b1 = new BodyOfWater(erfan, 70, 300, false);
         System.out.println("here");
     }
 }
