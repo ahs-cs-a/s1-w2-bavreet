c9114887fb0723f70dbb6d305887892abddc930a
bavreet <bavreets25@ausdk12.org>
2024-08-28 08:47:01 -0700

    Update BodyOfWaterMain.java

--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,7 +1,12 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater(lake tahoe, 20, 60, true);
+        BodyOfWater b = new BodyOfWater(n:"lake tahoe, 20, 60, true");
         BodyOfWater b1 = new BodyOfWater(erfan, 70, 300, false);
-        System.out.println("here");
+        System.out.println(b.getName());
+        System.out.prinyln(b.getavgDepth());
+        System.out.println(b.getavgDiameter());
+        System.out.println(b.getisSalt());
+        System.out.println(b1.getName());
+        System.out.println(b1.get
     }
 }
